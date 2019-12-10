28b86987c15c007757a538d65833dcfacfa0abd2
Anthony Hill <hillan1152@gmail.com>
2019-12-10 11:30:17 -0500

    PUT AND DELETE

--- Accounts/accountRouter.js
+++ Accounts/accountRouter.js
@@ -1,57 +1,81 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const knex = require('../data/dbConfig.js');
+const knex = require("../data/dbConfig.js");
 
+router.get("/", (req, res) => {
+  knex
+    .select("*")
+    .from("accounts")
+    .then(account => {
+      res.status(200).json(account);
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: "Error getting the account" });
+    });
+});
+router.get("/:id", (req, res) => {
+  knex
+    .select("*")
+    .from("accounts")
+    .where({ id: req.params.id })
+    .first()
+    .then(account => {
+      res.status(200).json(account);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "Error getting the account" });
+    });
+});
 
-router.get('/', (req, res) => {
-    knex.select('*').from('accounts')
+router.post("/", (req, res) => {
+  const accountData = req.body;
+
+  knex("accounts")
+    .insert(accountData, "id")
+    .then(ids => {
+      const id = ids[0];
+      return knex("accounts")
+        .select("name", "budget") // THIS MAKES SURE TO RETURN AN ENTIRE ARRAY OF YOU NEW INPUT
+        .where({ id })
         .then(account => {
-            res.status(200).json(account);
-        })
-        .catch(err => {
-            res.status(500).json({ errorMessage: 'Error getting the account'})
-        })
-})
-router.get('/:id', (req, res) => {
-    knex.select('*')
-        .from('accounts')
-        .where({ id: req.params.id })
-        .first()
-        .then(account => {
-            res.status(200).json(account);
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json({ errorMessage: 'Error getting the account'})
-        })
-})
+          res.status(201).json(account);
+        });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "Error creating this account." });
+    });
+});
 
-router.post('/', (req, res) => {
-    const accountData = req.body;
+router.put("/:id", (req, res) => {
+  const changes = req.body;
+  const { id } = req.params;
 
-    knex("accounts")
-        .insert(accountData, "id")
-        .then(ids => {
-            const id = ids[0]
-            return knex("accounts")
-                .select('name', 'budget') // THIS MAKES SURE TO RETURN AN ENTIRE ARRAY OF YOU NEW INPUT
-                .where({ id })
-                .then(account => {
-                    res.status(200).json(account)
-            })
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json({ errorMessage: 'Error creating this account.'})
-        })
-})
+  knex("accounts")
+    .where({ id })
+    .update(id, changes)
+    .then(count => {
+      res.status(200).json({ message: `${count} record(s) updated.` });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "Error updating this account." });
+    });
+});
 
-router.post('/:id', (req, res) => {
-    const accountData = req.body;
-    const { id } = req.params;
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
 
-    knex('accounts').update(id, accountData).then().catch()
-})
-
+  knex("accounts")
+    .where({ id })
+    .delete()
+    .then(count => {
+      res.status(204).json({ message: `${count} record(s) deleted.` });
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: "Error removing this account." });
+    });
+});
 
 module.exports = router;
